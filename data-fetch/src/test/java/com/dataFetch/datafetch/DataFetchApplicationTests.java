464c027433ef2f0a8390d492a79fe4ad182decdb
mchhade <49597971+mchhade@users.noreply.github.com>
2021-06-03 10:07:25 +0300

    Create DataFetchApplicationTests.java

---
+++ data-fetch/src/test/java/com/dataFetch/datafetch/DataFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataFetch.datafetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
