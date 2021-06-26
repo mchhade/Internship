e17922942fdc06210a9b1b565cdba119cd102c88
mchhade <49597971+mchhade@users.noreply.github.com>
2021-06-26 11:48:58 +0300

    Update app.module.ts

--- Employee/src/app/app.module.ts
+++ Employee/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PersonListComponent } from './person-list/person-list.component';
+import { PopupComponent } from './component/popup/popup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PersonListComponent,
+      PopupComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
